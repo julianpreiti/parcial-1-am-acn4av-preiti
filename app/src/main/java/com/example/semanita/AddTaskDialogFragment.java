93d7e0ebcf4668a593b1fb01950f4dc74ad52b80
Julian Preiti <58276522+julianpreiti@users.noreply.github.com>
2025-07-01 00:11:42 -0300

    chore: update unused 2

--- app/src/main/java/com/example/semanita/AddTaskDialogFragment.java
+++ app/src/main/java/com/example/semanita/AddTaskDialogFragment.java
@@ -2,7 +2,6 @@ package com.example.semanita;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
